8d60478abd0b8b2b03bb6d854016fc5e5200a3ac
Stephen McGruer <smcgruer@chromium.org>
2021-05-06 15:56:49 -0400

    Add initial pr-handler/ subpage

---
+++ pr/spc-handler/pr.js
@@ -0,0 +1,81 @@
+/* exported createPaymentCredential */
+/* exported onBuyClicked */
+
+const textEncoder = new TextEncoder();
+
+/**
+ * Initializes the payment request object.
+ * @return {PaymentRequest} The payment request object.
+ */
+async function buildPaymentRequest() {
+  if (!window.PaymentRequest) {
+    return null;
+  }
+
+  let request = null;
+
+  try {
+    // Documentation:
+    // https://github.com/rsolomakhin/secure-payment-confirmation
+    const supportedInstruments = [{
+      supportedMethods: ["https://hilarious-historical-spectrum.glitch.me/method-manifest"]
+    }];
+
+    const details = {
+      total: {
+        label: 'Total',
+        amount: {
+          currency: 'USD',
+          value: '0.01',
+        },
+      },
+    };
+
+    request = new PaymentRequest(supportedInstruments, details);
+  } catch (err) {
+    error(err);
+  }
+
+  return request;
+}
+
+async function onBuyClicked() {
+  if (!window.PaymentRequest) {
+    error('PaymentRequest API is not supported.');
+    return;
+  }
+
+  const request = await buildPaymentRequest();
+  if (!request)
+    return;
+
+  try {
+    const instrumentResponse = await request.show();
+    await instrumentResponse.complete('success')
+    info(JSON.stringify(instrumentResponse, undefined, 2));
+  } catch (err) {
+    error(err);
+  }
+}
+
+async function checkCanMakePayment() {
+  if (!window.PaymentRequest) {
+    error('PaymentRequest API is not supported.');
+    return;
+  }
+  try {
+    const request = await buildPaymentRequest();
+    if (!request)
+      return;
+    const result = await request.canMakePayment();
+    if (result) {
+      info("Can make payment.");
+    } else {
+      error("Cannot make payment.");
+    }
+  } catch (err) {
+    error(err);
+  }
+}
+
+checkCanMakePayment();
