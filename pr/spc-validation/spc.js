80e8e5559551662b97d061b844911a261d61c629
Stephen McGruer <smcgruer@chromium.org>
2021-07-14 13:01:26 -0400

    Draft of SPC validation logic

---
+++ pr/spc-validation/spc.js
@@ -0,0 +1,167 @@
+const textEncoder = new TextEncoder();
+
+/**
+ * Creates a credential for SPC, and stores the credential ID in localStorage
+ * for later use when authenticating.
+ */
+async function createPaymentCredential(windowLocalStorageIdentifier) {
+  const rp = {
+    id: 'stephenmcgruer.github.io',
+    name: 'Stephen McGruer',
+  };
+  const instrument = {
+    displayName: 'Troy ····',
+    icon: 'https://stephenmcgruer.github.io/web-payments-demos/pr/spc/troy.png',
+  };
+  const pubKeyCredParams = [{
+    type: 'public-key',
+    alg: -7,  // ECDSA, not supported on Windows.
+  }, {
+    type: 'public-key',
+    alg: -257,  // RSA, supported on Windows.
+  }];
+  const authenticatorSelection = {
+    userVerification: 'required',
+  };
+  const payment = {
+    rp,
+    instrument,
+    challenge: textEncoder.encode('Transaction approval challenge'),
+    pubKeyCredParams,
+    authenticatorSelection,
+  };
+  try {
+    const credential = await navigator.credentials.create({payment});
+    window.localStorage.setItem(
+        windowLocalStorageIdentifier,
+        btoa(String.fromCharCode(...new Uint8Array(credential.rawId))));
+    info(windowLocalStorageIdentifier + ': Credential ' +
+         window.localStorage.getItem(windowLocalStorageIdentifier) +
+         ' enrolled.');
+  } catch (err) {
+    error(err);
+  }
+}
+
+async function onBuyClicked(windowLocalStorageIdentifier) {
+  if (!window.PaymentRequest) {
+    error('PaymentRequest API is not supported.');
+    return;
+  }
+
+  const request = await buildPaymentRequest(window.localStorage.getItem(windowLocalStorageIdentifier));
+  if (!request)
+    return;
+
+  try {
+    const instrumentResponse = await request.show();
+    await instrumentResponse.complete('success')
+    info(windowLocalStorageIdentifier + ': ' + JSON.stringify(instrumentResponse, undefined, 2));
+
+    await validate(instrumentResponse.details);
+  } catch (err) {
+    error(err);
+  }
+}
+
+/**
+ * Builds a PaymentRequest object for SPC authentication and returns it.
+ */
+async function buildPaymentRequest(credentialId) {
+  if (!window.PaymentRequest) {
+    return null;
+  }
+
+  let request = null;
+
+  try {
+    const supportedInstruments = [{
+      supportedMethods: 'secure-payment-confirmation',
+      data: {
+        action: 'authenticate',
+        credentialIds: [Uint8Array.from(atob(credentialId), c => c.charCodeAt(0))],
+        networkData: textEncoder.encode('network_data'),
+        timeout: 60000,
+        fallbackUrl: ''
+      },
+    }];
+
+    const details = {
+      total: {
+        label: 'Total',
+        amount: {
+          currency: 'USD',
+          value: '0.01',
+        },
+      },
+    };
+
+    request = new PaymentRequest(supportedInstruments, details);
+  } catch (err) {
+    error(err);
+    throw err;
+  }
+
+  return request;
+}
+
+/**
+ * Example code for validating the payments parts of the response returned by SPC.
+ *
+ * This is an EXAMPLE ONLY and should not be used in production code! For a
+ * start, general WebAuthn validation should also be done; see
+ * https://w3c.github.io/webauthn/#sctn-verifying-assertion
+ */
+async function validate(details) {
+  let authenticatorData, clientDataJSON, signature, challenge;
+
+  if (details.constructor === PublicKeyCredential) {
+    // WebAuthn-style response (>= M93).
+
+    authenticatorData = details.response.authenticatorData;  // ArrayBuffer
+    clientDataJSON = details.response.clientDataJSON;  // ArrayBuffer
+    signature = details.response.signature;  // ArrayBuffer
+
+    const clientData = JSON.parse(arrayBufferToString(clientDataJSON));
+    challenge = clientData.challenge
+
+    // assert(challenge == base64url encoding of input challenge to SPC).
+
+    const paymentInfo = clientData.payment;
+
+    // TODO: Fix this.
+    // assert(paymentInfo.??? is ???)
+    // assert(paymentInfo.total.currency is as expected)
+    // assert(paymentInfo.total.amount is as expected)
+  } else {
+    // Legacy SPC response (<M93).
+
+    authenticatorData = details.info.authenticator_data;  // base64url-encoded
+    clientDataJSON = details.info.client_data_json;  // base64url-encoded
+    signature = details.signature;  // base64url-encoded
+
+    const clientData = JSON.parse(atob(clientDataJSON));
+    challenge = clientData.challenge;
+
+    const spcChallenge = JSON.parse(details.challenge);
+    // assert(spcChallenge.networkData == arrayBufferToString(input challenge to SPC))
+
+    const paymentInfo = spcChallenge.merchantData;
+
+    // assert(paymentInfo.merchantOrigin is as expected)
+    // assert(paymentInfo.total.currency is as expected)
+    // assert(paymentInfo.total.amount is as expected)
+  }
+
+  // cData == clientDataJSON as an arraybuffer
+  // 
+  // Let hash be the result of computing a hash over the cData using SHA-256.
+  //   let hash = await crypto.subtle.digest("SHA-256", cData)
+  // Using credentialPublicKey, verify that sig is a valid signature over the binary concatenation of authData and hash.
+  //   let concated = concatBuffers(authenticatorData, hash)
+  //   // TODO: Validate signature.
+}
+
+function arrayBufferToString(buffer) {
+  return String.fromCharCode(...new Uint8Array(input));
+}
