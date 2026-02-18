c5e2a10260dc6355b87d7752b4e3661377a08a35
Stephen McGruer <smcgruer@google.com>
2026-02-18 15:14:28 -0500

    Implement

--- pr/webauthn/pr.js
+++ pr/webauthn/pr.js
@@ -1,11 +1,27 @@
-let credentialId;
+const windowLocalStorageIdentifier = "Credential #1"
 
 document.getElementById('createButton').addEventListener('click', async () => {
-  const publicKeyCredential = await createCredential(/*setPaymentExtension=*/false);
+  publicKeyCredential = await createCredential(/*setPaymentExtension=*/false);
   console.log(publicKeyCredential);
   info('enrolled: ' + objectToString(publicKeyCredential));
+
+  window.localStorage.setItem(windowLocalStorageIdentifier,
+    arrayBufferToBase64(publicKeyCredential.rawId));
+  window.parent.postMessage({
+    type: 'enrollment',
+    credential": publicKeyCredential.id,
+  }, '*');
+
   document.getElementById('triggerSpcButton').disabled = false;
 });
 
 document.getElementById('triggerSpcButton').addEventListener('click', async () => {
+  const request = await createSPCPaymentRequest({
+    credentialIds: [base64ToArray(window.localStorage.getItem(windowLocalStorageIdentifier))],
+  });
+  const instrumentResponse = await request.show();
+  await instrumentResponse.complete('success')
+  console.log(instrumentResponse);
+  info(windowLocalStorageIdentifier + ' payment response: ' +
+    objectToString(instrumentResponse));
 });
